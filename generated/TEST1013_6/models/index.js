ba175a4059595117a47b02208ae6d270390dcd11
Dave Goldberg <davejonathangoldberg@gmail.com>
2014-10-15 20:52:51 -0400

    success :) auto deploy worked.

--- generated/TEST1013_6/models/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function Models() {
-this.speakers = require('./speakers');
-this.topics = require('./topics');
-this.email_addresses = require('./email_addresses');
-this.topics = require('./topics');
-this.speakers = require('./speakers');
-this.details = require('./details');
-
-}
--- generated/test1014_03/meta.js
+++
@@ -1,8 +0,0 @@
-var meta = {};
-meta['root'] = {'childResources' : [ 'speakers','topics','email_addresses','details', ]};
-meta['speakers'] = {'childResources' : ['topics','email_addresses'], 'parentResources' : ['topics']};
-meta['topics'] = {'childResources' : ['speakers'], 'parentResources' : ['speakers']};
-meta['email_addresses'] = {'childResources' : [], 'parentResources' : ['speakers']};
-meta['details'] = {'childResources' : [], 'parentResources' : []};
-meta['validRoutes'] = ['speakers','speakerstopics','speakersemail_addresses','topics','topicsspeakers','details'];
-module.exports = meta;
