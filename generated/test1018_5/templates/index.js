1d71a8c0fe081eb27aed989f0b6d9f2128de059c
Dave Goldberg <davejonathangoldberg@gmail.com>
2014-10-19 11:37:02 -0400

    debug mkdir

--- generated/test1018_5/templates/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function Templates() {
-this.speakers = require('./speakers.js');
-this.topics = require('./topics.js');
-this.email_addresses = require('./email_addresses.js');
-this.topics = require('./topics.js');
-this.speakers = require('./speakers.js');
-this.details = require('./details.js');
-
-}
