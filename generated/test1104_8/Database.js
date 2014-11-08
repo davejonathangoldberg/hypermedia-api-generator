193823d890eebdaf296e5ab3a730be478b5c0ddf
Dave Goldberg <davejonathangoldberg@gmail.com>
2014-11-07 21:33:25 -0500

    webhooks and db work

--- generated/test1104_8/Database.js
+++
@@ -1,9 +0,0 @@
-// Database.js
-module.exports = function Database(configuration) {
-  var mongoose = require('mongoose');
-  var mongooseUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL || 'mongodb://' + 
-  configuration.host + configuration.database;
-  console.log('mongooseURI = ' + mongooseUri);
-  mongoose.connect(mongooseUri);
-};
--- generated/todo_api/templates/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function Templates() {
-this.items = require('./items.js');
-this.assignees = require('./assignees.js');
-this.projects = require('./projects.js');
-this.items = require('./items.js');
-
-}
