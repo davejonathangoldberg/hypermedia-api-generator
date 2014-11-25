9ddb08b75b37eb007426161c5dd5cdcc5d4c168c
Dave Goldberg <davejonathangoldberg@gmail.com>
2014-11-24 20:33:20 -0500

    local success push

--- generated/todo_api_1124_1/models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function Models() {
-this.items = require('./items');
-this.assignees = require('./assignees');
-this.projects = require('./projects');
-this.items = require('./items');
-
-}
